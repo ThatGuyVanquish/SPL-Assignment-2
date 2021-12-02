a64565bfaf0c8c1916bd33d3b50cd539dabb8783
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-12-02 22:04:48 +0200

    Model

--- src/main/java/bgu/spl/mics/application/objects/Model.java
+++ src/main/java/bgu/spl/mics/application/objects/Model.java
@@ -6,5 +6,25 @@ package bgu.spl.mics.application.objects;
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class Model {
+    private String name;
+    private Data data;
+    private Student student;
+    private enum status {PreTrained, Training, Trained, Tested};
+    private enum results {None, Good, Bad};
+    private status currentStatus;
+    private results currentResult;
 
+    public Model(String Name, Data data, Student student) {
+        this.name = name;
+        this.data = data;
+        this.student = student;
+        this.currentStatus = status.PreTrained;
+        this.currentResult = results.None;
+    }
+
+    public String getName() { return this.name;}
+    public Data getData() { return this.data;}
+    public Student getStudent() { return this.student;}
+    public status getStatus() { return this.currentStatus;}
+    public results getResult() { return this.currentResult;}
 }
