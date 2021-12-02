ac40d304dc305ec340fe2c61be041e142c541c97
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-12-02 21:50:51 +0200

    fixes shit

--- src/test/java/FutureTest.java
+++ src/test/java/FutureTest.java
@@ -1,16 +1,22 @@
-package bgu.spl.mics;
+
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import bgu.spl.mics.Future;
+import bgu.spl.mics.application.objects.Model;
 import static org.junit.Assert.*;
 
 public class FutureTest {
 
+    private Future<Model> preTrained;
+    private Future<Model> trained;
     @Before
     public void setUp() throws Exception {
-        Future ftr = new Future();
+        preTrained = new Future();
+        trained = new Future();
+
     }
 
     @After
@@ -20,7 +26,6 @@ public class FutureTest {
     @Test
     public void get() {
         try {
-            T obj = ftr.get();
 
         }
 
