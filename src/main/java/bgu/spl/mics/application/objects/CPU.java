78d4fd6b5f725ea09c2595dd09bda065371f2d72
TomOr37 <81587688+TomOr37@users.noreply.github.com>
2021-12-03 10:14:57 +0200

    CPU Frame

--- src/main/java/bgu/spl/mics/application/objects/CPU.java
+++ src/main/java/bgu/spl/mics/application/objects/CPU.java
@@ -1,9 +1,29 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.Vector;
+
 /**
  * Passive object representing a single CPU.
  * Add all the fields described in the assignment as private fields.
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class CPU {
+    private int cores;
+    private Data data;
+    private  DataBatch dataBatch;
+    public CPU(int cores,Data data,DataBatch dataBatch){
+        this.data = data;
+        this.cores= cores;
+        this.dataBatch = dataBatch;
+    }
+    public void Train(){
+
+    }
+
+    private void SentToCluster(){
+
+    }
+
+
+
 }
