9c692679900779cc066f2077e8aba8b1637e78f0
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-12-11 21:27:51 +0200

    Update TrainModelEvent.java
    Cosmetics

--- src/main/java/bgu/spl/mics/TrainModelEvent.java
+++ src/main/java/bgu/spl/mics/TrainModelEvent.java
@@ -3,10 +3,14 @@ package bgu.spl.mics;
 import bgu.spl.mics.application.objects.Model;
 
 public class TrainModelEvent implements Event<Model>{ // Not sure Event<Model> is the correct implementation
+
     private Model model;
-    public TrainModelEvent(Model model){this.model = model;}
+
+    public TrainModelEvent(Model model){
+        this.model = model;
+    }
 
     public Model getModel() {
-        return model;
+        return this.model;
     }
 }
