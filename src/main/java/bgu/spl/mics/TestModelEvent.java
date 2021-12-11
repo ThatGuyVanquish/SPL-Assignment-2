c99130b4ddecf076c26ef58e5dcc7a911359dd3f
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-12-11 21:28:05 +0200

    Update TestModelEvent.java
    Added basic methods

--- src/main/java/bgu/spl/mics/TestModelEvent.java
+++ src/main/java/bgu/spl/mics/TestModelEvent.java
@@ -3,4 +3,14 @@ package bgu.spl.mics;
 import bgu.spl.mics.application.objects.Model;
 
 public class TestModelEvent implements Event<Model>{ // Not sure Event<Model> is the correct implementation
+
+    private Model model;
+
+    public TestModelEvent(Model model) {
+        this.model = model;
+    }
+
+    public Model getModel() {
+        return this.model;
+    }
 }
