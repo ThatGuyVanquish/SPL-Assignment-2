be9485aff52156b0acf75269968b6b7a7efbec56
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-12-12 11:34:41 +0200

    Update Cluster.java
    Added methods and placeholders for cluster methods

--- src/main/java/bgu/spl/mics/application/objects/Cluster.java
+++ src/main/java/bgu/spl/mics/application/objects/Cluster.java
@@ -1,6 +1,8 @@
 package bgu.spl.mics.application.objects;
 
 
+import bgu.spl.mics.MessageBusImpl;
+
 /**
  * Passive object representing the cluster.
  * <p>
@@ -10,12 +12,25 @@ package bgu.spl.mics.application.objects;
  */
 public class Cluster {
 
-	public static final Cluster clusterInst = new Cluster();
+	// Should Cluster also have access to CPUs? Or at least to the message bus
+
+	private static final Cluster CLUSTER = new Cluster();
+	private static final MessageBusImpl MESSAGE_BUS = MessageBusImpl.getInstance();
 	/**
      * Retrieves the single instance of this class.
      */
 	public static Cluster getInstance() {
-		return clusterInst;
+		return CLUSTER;
 	}
-    public void AddProcessedData(DataBatch dataBatch){}
+
+	public void processData(int maxBatches) {
+		// Should look at all CPUs and send appropriate amount of batches to CPUs
+	}
+
+    public void addProcessedData(DataBatch dataBatch){
+		if (dataBatch != null)
+		{
+			dataBatch.getData().processData();
+		}
+	} // What is this for? Should this send the batch back to the GPU?
 }
