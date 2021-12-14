fc55d8e9e2cc0df4d9656154bc090e95550dc056
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-12-14 11:54:29 +0200

    Create ThinkingAboutMessagePriorityQueue.java
    I didn't fully understand how do we prioritize message classes via awaitmessage

---
+++ src/main/java/bgu/spl/mics/application/ThinkingAboutMessagePriorityQueue.java
@@ -0,0 +1,12 @@
+package bgu.spl.mics.application;
+
+import bgu.spl.mics.Message;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class ThinkingAboutMessagePriorityQueue
+{
+
+    private PriorityBlockingQueue<Message> messageQueue;
+    //probably irrelevant will ask you later
+}
